69e77fa5486a91f124543734b00833c6079f272e
sso214 <sojam214@gmail.com>
2023-06-17 20:26:39 +0900

    study : inflearn 강의 5-1

--- inflearn/5-1.두 배열 합치기.js
+++ inflearn/5-1.두 배열 합치기.js
@@ -18,7 +18,72 @@
 */
 
 /*나의 답변*/
-const solution = (arr1, ...arr) => arr1.concat(...arr).sort((a,b) => a-b);
+const solution = (arr1, ...arr) => arr1.concat(...arr).sort((a, b) => a - b);
 
-solution([1,3,5], [2,3,6,7,9]);
-solution([1,3,5], [2,3,6,7,9], [3,4,5,6,7,8,9]);
+solution([1, 3, 5], [2, 3, 6, 7, 9]);
+solution([1, 3, 5], [2, 3, 6, 7, 9], [3, 4, 5, 6, 7, 8, 9]);
+
+
+//수업에서의 답
+//sort 함수를 호출 => nlogn
+function solution2(arr1, arr2) {
+    const arrLength = arr1.length + arr2.length;
+    let list = [];
+    let p1 = 0;
+    let p2 = 0;
+
+    for (let i = 0; i < arrLength; i++) {
+
+        if (arr1[p1] === arr2[p2]) {
+            list.push(...[arr1[p1], arr2[p2]]);
+            p1++;
+            p2++;
+            continue;
+        }
+
+        if (arr1[p1] < arr2[p2]) {
+            list.push(arr1[p1]);
+            p1++;
+            continue;
+        }
+
+        if (arr1[p1] > arr2[p2]) {
+            list.push(arr2[p2]);
+            p2++;
+            continue;
+        }
+
+        if (p1 <= arr1.length) {
+            list.push(...arr2.slice(p2));
+            break;
+        }
+
+        if (p2 <= arr2.length) {
+            list.push(...arr1.slice(p1));
+            break;
+        }
+    }
+
+    return list;
+}
+
+solution2([1, 3, 5], [2, 3, 6, 7, 9]);
+
+
+function solution3(arr1, arr2) {
+    let list = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < n && p2 < m) {
+        if (arr1[p1] <= arr2[p2]) list.push(arr1[p1++]);
+        else list.push(arr2[p2++]);
+    }
+    while (p1 < n) list.push(arr1[p1++]);
+    while (p2 < m) list.push(arr2[p2++]);
+    return list;
+}
+
+solution3([1, 3, 5], [2, 3, 6, 7, 9]);
